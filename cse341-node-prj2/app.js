caefbb899b4164ab55d4d5cfa5c15805ea1677be
aalund2013 <ran08001@byui.edu>
2022-05-14 10:19:15 -0600

    -/

--- cse341-node-prj2/app.js
+++ cse341-node-prj2/app.js
@@ -18,19 +18,6 @@ require('./models/passport-setup')(passport);
 const port = process.env.PORT || 8080;
 const app = express();
 
-// const multer = require('multer');
-  
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads')
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, file.fieldname + '-' + Date.now())
-//     }
-// });
-  
-// const upload = multer({ storage: storage });
-// const imgModel = require('./models');
 app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
