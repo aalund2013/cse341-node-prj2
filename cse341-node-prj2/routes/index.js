558e1bf90251d0d04f2377306d0baf49eb2a5c16
aalund2013 <ran08001@byui.edu>
2022-05-14 10:53:27 -0600

    protect swagger

--- cse341-node-prj2/routes/index.js
+++ cse341-node-prj2/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const swaggerUi = require('swagger-ui-express');
+const docRoute = require('./api-docs');
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 router.use('/auth', require('./auth'));
 router.use('/users', ensureAuth, require('./users'));
 router.use('/posts', ensureAuth, require('./posts'));
+router.use('/api-docs', ensureAuth, swaggerUi.serve, swaggerUi.setup(docRoute));
 
 // Login/Landing page
 // Route GET /
