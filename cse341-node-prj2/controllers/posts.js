6e1b0b855e8231579d1d8047f22b53e98fabdad3
aalund2013 <ran08001@byui.edu>
2022-05-19 22:58:09 -0600

    oops s

--- cse341-node-prj2/controllers/posts.js
+++ cse341-node-prj2/controllers/posts.js
@@ -28,9 +28,10 @@ const get_post_by_id = async (req, res, next) => {
 };
 
 // Get all posts by specific user
+// #swagger.tags = ['Posts']
 const get_users_posts = async (req, res) => {
     try{
-    const posts = await Posts.find( {"users": req.params.user} );
+    const posts = await Posts.find( {"user": req.params.user} );
     res.json(posts);
     } catch(err){
         res.json({message:err});
