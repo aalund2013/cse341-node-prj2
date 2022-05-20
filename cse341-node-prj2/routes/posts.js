6003a441b739cdcadf45028ffb3f3aa4b538ff48
aalund2013 <ran08001@byui.edu>
2022-05-20 09:10:00 -0600

    change order

--- cse341-node-prj2/routes/posts.js
+++ cse341-node-prj2/routes/posts.js
@@ -4,11 +4,12 @@ var router = require('express').Router();
 // GET ALL POSTS
 router.get("/", posts.get_posts);
 
+// GET POST BY USER
+router.get("/:user", posts.get_users_posts);
+
 // GET SPECIFIC POST
 router.get("/:postId", posts.get_post_by_id);
 
-// GET POST BY USER
-router.get("/:user", posts.get_users_posts);
 
 // POST A NEW POST
 router.post("/", posts.new_post);
